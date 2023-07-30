90ea3fa29fd582d46f26a8235c60fd7418b0a834
Eddie <Eddie@racer-tech.com>
2023-07-30 16:50:11 +0000

    add kenel message

--- helloworld.c
+++ helloworld.c
@@ -2,8 +2,17 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+static int log_level = 4; // 假設模塊定義了自己的printk參數，初始值為4
+
+module_param(log_level, int, S_IRUGO); // 定義printk參數
+
 static int __init helloworld_init(void) {
-    printk(KERN_INFO "Hello, World!\n");
+    printk(KERN_INFO "kernel INFO - Hello, World!\n");
+    printk("Hello world - origin printk\n");
+    pr_info("pr_info - Hello, World!\n");
+    printk(KERN_ERR "kernel ERR - Hello, World!\n");
+    printk(KERN_EMERG "kernel EMERG - Hello, World!\n");
+
     return 0; // 非零值表示初始化失敗
 }
 
